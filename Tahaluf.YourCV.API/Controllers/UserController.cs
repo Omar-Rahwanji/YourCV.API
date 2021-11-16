1826f55c6572307cf72d59a2d4428674b89a6e82
Rahaf Almasri <rahafalmasri65@gmail.com>
2021-11-16 19:14:39 +0200

    finched Add-Image rahaf

--- Tahaluf.YourCV.API/Controllers/UserController.cs
+++ Tahaluf.YourCV.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Tahaluf.YourCV.Core.Data;
@@ -38,7 +39,7 @@ namespace Tahaluf.YourCV.API.Controllers
         }
 
         [HttpPut]
-        [Route("[action]")]
+        [Route("UpdateUser")]
         [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public bool UpdateUser([FromBody] User user)
@@ -56,14 +57,53 @@ namespace Tahaluf.YourCV.API.Controllers
         }
 
 
-        [HttpPost]
-        [Route("[action]")]
+        [HttpGet]
+        [Route("GetUserById/{id}")]
         [ProducesResponseType(typeof(List<User>), StatusCodes.Status200OK)]
 
-        public List<User> GetUserById(User user)
+        public List<User> GetUserById(int id)
         {
-            return userService.GetUserById(user);
+            return userService.GetUserById(id);
         }
 
+
+        
+        [HttpPost]
+        [Route("upload")]
+        public User Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                byte[] fileContent;
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    fileContent = ms.ToArray();
+                }
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+
+
+
+
+                string attachmentFileName = $"{Guid.NewGuid().ToString("N")}_{fileName}.{Path.GetExtension(file.FileName).Replace(".", "")}";
+                var fullPath = Path.Combine("C:\\Users\\DELL\\Desktop\\YourCv\\Tahaluf.YourCV.API\\Properties\\assets\\" + "images\\customers\\",attachmentFileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return new User { PersonalPhoto = attachmentFileName };
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+
+
+
+
+        }
     }
 }
+
