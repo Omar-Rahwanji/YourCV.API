fe6f59ffadc52d7e6a35690972fbe94a65a718e8
Omar Rahwanji <rahwanjiomar@gmail.com>
2021-11-16 19:13:11 +0200

    finished omar branch

--- Tahaluf.YourCV.API/Controllers/UserController.cs
+++ Tahaluf.YourCV.API/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Tahaluf.YourCV.Core.Data;
@@ -21,7 +22,36 @@ namespace Tahaluf.YourCV.API.Controllers
         }
 
         [HttpPost]
-        [Route("[action]")]
+        [Route("upload")]
+        public User Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                byte[] fileContent;
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    fileContent = ms.ToArray();
+                }
+                var fileName = Path.GetFileNameWithoutExtension(file.FileName);
+
+                string attachmentFileName = $"{Guid.NewGuid().ToString("N")}_{fileName}.{Path.GetExtension(file.FileName).Replace(".", "")}";
+                var fullPath = Path.Combine("C:\\Users\\omarr\\Documents\\YourCV.API\\Tahaluf.YourCV.API\\assets\\images\\customers\\", attachmentFileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                return new User { PersonalPhoto = attachmentFileName };
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+
+        [HttpPost]
+        [Route("CreateUser")]
         [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public bool CreateUser([FromBody] User user)
